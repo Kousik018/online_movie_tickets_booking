5e4ecce464158a2a0875593ebc117f5c504c1972
Kousik018 <159710742+Kousik018@users.noreply.github.com>
2025-10-30 12:12:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./Components/Navbar";
 import MovieList from "./Components/MovieList";
